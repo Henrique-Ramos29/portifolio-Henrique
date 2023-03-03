17b75e8583a7cd89a1825a3f8aedc041654e8e98
Henrique-Ramos29 <120992364+Henrique-Ramos29@users.noreply.github.com>
2023-03-03 10:05:36 -0300

    atualização de arquivos

--- validate.js
+++ validate.js
@@ -1,4 +1,3 @@
-
 const nameInput = document.querySelector('#nome');
 const counter = document.querySelector('.spam');
 
@@ -8,35 +7,29 @@ const counter2 = document.querySelector('.spam2');
 const textArea3 = document.querySelector('#mensagem');
 const counter3 = document.querySelector('.spam3');
 
-
-
-  nameInput.addEventListener('input', () => {
-
+nameInput.addEventListener('input', () => {
     if (nameInput.value.length < 50) {
         counter.innerText = nameInput.value.length;
     } else {
         counter.innerText = 'Max';
         nameInput.value = nameInput.value.substr(0, 50);
     }
-}) 
+});
 
 nameInput2.addEventListener('input', () => {
-
     if (nameInput2.value.length < 50) {
         counter2.innerText = nameInput2.value.length;
     } else {
         counter2.innerText = 'Max';
         nameInput2.value = nameInput2.value.substr(0, 50);
     }
-}) 
+});
 
 textArea3.addEventListener('input', () => {
-
-    if (textArea3.value.length < 150) {
+    if (textArea3.value.length < 200) {
         counter3.innerText = textArea3.value.length;
     } else {
         counter3.innerText = 'Max';
-        textArea3.value = textArea3.value.substr(0, 150);
+        textArea3.value = textArea3.value.substr(0, 200);
     }
-
-})
+});
